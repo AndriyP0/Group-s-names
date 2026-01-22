e5b8f7ff54aa86b87a7d5323f6b5bf78fba28683
AndriyP0 <andreopopovich@gmail.com>
2026-01-23 01:08:25 +0200

    add all file

---
+++ src/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const arrGroups = [
+  "FE2021-5",
+  "Fe2020-2",
+  "FD2021",
+  "fD2022-1",
+  "FE2020-3",
+  "FD2020/2",
+  "fe2021-3",
+  "FD2020$5",
+  "FE2008-52",
+  "FU2008-3",
+  "Fm2008-23",
+  "FM2022-1",
+];
+
+function checkGroups(groups) {
+const reg = /^(FD|FE|FM|fd|fe|fm)20\d{2}(-\d?)?$/;
+  groups.forEach((group) => {
+    if (reg.test(group)) {
+      console.log(`Success: ${group}`);
+    } else {
+      console.log(`Error: ${group}`);
+    }
+  });
+}
+
+checkGroups(arrGroups);
